c6cd9bfdf0639f0d9715ef447f0fccc769a9cc94
Musa Uçar <92794324+musaucar@users.noreply.github.com>
2022-09-15 17:33:10 +0300

    react router update
    react router update error

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route, Link, NavLink } from "react-rou
 import Home from './components/Home'
 import Users from './components/Users'
 import About from './components/About'
+import Error404 from './components/Error404.js'
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
             <Route path="/" exact component={Home}/>
             <Route path="/about" component={About}/>
             <Route path="/users" component={Users}/>
+            <Route path="*" component={Error404}/>
           </Switch>
         </div>
       </Router>
